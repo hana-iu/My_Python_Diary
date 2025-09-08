71a3d9f1d850082adc8f6e1b6bf1237008280c9a
hana-iu <hana.yasser.20007@gmail.com>
2025-09-08 17:25:05 +0300

    "check if password is correct!"

---
+++ pass_check.py
@@ -0,0 +1,6 @@
+password = "lily_20"
+entered_pass = input("Enter the password: ")
+while password != entered_pass: 
+   entered_pass = input("Incorrect password, please enter it again: ")
+
+print("Password is correct!")
